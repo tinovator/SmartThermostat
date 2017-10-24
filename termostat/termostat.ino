e615a33f32aab84314fd8d2ef794bf35181ea9bb
tinovator <tinoherljevic@gmail.com>
2017-10-24 15:51:51 +0200

    Added logic for setting temperature and updating room temp. every 15 seconds.

--- termostat/termostat.ino
+++ termostat/termostat.ino
@@ -5,50 +5,60 @@
 */
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <LiquidCrystal.h>
 #define ONE_WIRE_BUS 2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+LiquidCrystal lcd(11, 12, 5, 6, 7, 8);
 
 int relay = 4;
 int downBtn = 2;
 int upBtn = 3;
-int upPressed, downPressed;
+volatile int upPressed, downPressed = 0;
 double defaultTemp = 22.00;
 double tempStep = 0.50;
 double currentTemp, setTemp, average;
+unsigned long oldStamp = 0;
+const long interval = 15000;
 
 void setup() {
   Serial.begin(9600);
+  lcd.begin(16, 2);
   pinMode(relay, OUTPUT);
   digitalWrite(relay, LOW);
   pinMode(downBtn, INPUT_PULLUP);
   pinMode(upBtn, INPUT_PULLUP);
   setTemp = defaultTemp;
   sensors.begin();
+  attachInterrupt(0, selectISR, LOW);
+  attachInterrupt(1, selectISR, LOW);
 }
 
 void loop() {
+
+  //check temperature every 15 seconds
+  unsigned long currentStamp = millis();
+  if (currentStamp - oldStamp >= interval) {
+    oldStamp = currentStamp;
+    getTemperature();
+  }
+}
+
+void selectISR() {
   upPressed = digitalRead(upBtn);
   downPressed = digitalRead(downBtn);
 
   if (upPressed == LOW) {
     setTemp = setTemp + tempStep;
+    delay(300);
+    getTemperature();
   }
   else if (downPressed == LOW) {
     setTemp = setTemp - tempStep;
+    delay(300);
+    getTemperature();
   }
 
-
-}
-
-void compareTemp() {
-  if (setTemp < currentTemp) {
-    turnOff();
-  }
-
-  else if (setTemp > currentTemp) {
-    turnOn();
-  }
 }
 
 void getTemperature() {
@@ -57,10 +67,21 @@ void getTemperature() {
   for (int i = 0; i < 3; i++) {
     sensors.requestTemperatures();
     average += sensors.getTempCByIndex(0);
-    delay(50);
+    delay(10);
   }
   currentTemp = average / 3.0;
+  compareTemperature();
 }
+
+void compareTemperature() {
+  if (setTemp < currentTemp) {
+    turnOff();
+  }
+  if (setTemp > currentTemp) {
+    turnOn();
+  }
+}
+
 
 void turnOn() {
   digitalWrite(relay, HIGH);
@@ -69,3 +90,4 @@ void turnOn() {
 void turnOff() {
   digitalWrite(relay, LOW);
 }
+
