b7b44e2eefd970076c22decb7d14012d6d39bff0
tinovator <tinoherljevic@gmail.com>
2017-10-24 23:22:29 +0200

    Changed logic for setting temperature.

--- termostat/termostat.ino
+++ termostat/termostat.ino
@@ -6,7 +6,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <LiquidCrystal.h>
-#define ONE_WIRE_BUS 2
+#define ONE_WIRE_BUS 5
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 LiquidCrystal lcd(11, 12, 5, 6, 7, 8);
@@ -19,7 +19,7 @@ double defaultTemp = 22.00;
 double tempStep = 0.50;
 double currentTemp, setTemp, average;
 unsigned long oldStamp = 0;
-const long interval = 15000;
+const long interval = 5000;
 
 void setup() {
   Serial.begin(9600);
@@ -30,64 +30,88 @@ void setup() {
   pinMode(upBtn, INPUT_PULLUP);
   setTemp = defaultTemp;
   sensors.begin();
-  attachInterrupt(0, selectISR, LOW);
-  attachInterrupt(1, selectISR, LOW);
+
+  Serial.println("Initialized everything");
 }
 
 void loop() {
-
-  //check temperature every 15 seconds
-  unsigned long currentStamp = millis();
-  if (currentStamp - oldStamp >= interval) {
-    oldStamp = currentStamp;
-    getTemperature();
-  }
-}
-
-void selectISR() {
   upPressed = digitalRead(upBtn);
   downPressed = digitalRead(downBtn);
 
   if (upPressed == LOW) {
-    setTemp = setTemp + tempStep;
-    delay(300);
-    getTemperature();
+    tempUp();
   }
-  else if (downPressed == LOW) {
-    setTemp = setTemp - tempStep;
-    delay(300);
-    getTemperature();
+  if (downPressed == LOW) {
+    tempDown();
   }
-
+  //check temperature every 15 seconds
+  unsigned long currentStamp = millis();
+  if (currentStamp - oldStamp >= interval) {
+    oldStamp = currentStamp;
+    Serial.println("Getting temperature");
+    getTemperature();
+    compareTemperature();
+    Serial.print("currentTemp ");
+    Serial.println(currentTemp);
+    Serial.print("Set temperature ");
+    Serial.println(setTemp);
+  }
 }
 
+
+void tempDown() {
+  setTemp = setTemp - tempStep;
+  Serial.print("Step down");
+  delay(500);
+  getTemperature();
+  compareTemperature();
+  Serial.print("currentTemp ");
+  Serial.println(currentTemp);
+  Serial.print("Set temp ");
+  Serial.println(setTemp);
+}
+void tempUp() {
+  setTemp = setTemp + tempStep;
+  Serial.print("Step up");
+  delay(500);
+  getTemperature();
+  compareTemperature();
+  Serial.print("currentTemp ");
+  Serial.println(currentTemp);
+  Serial.print("Set temp ");
+  Serial.println(setTemp);
+}
 void getTemperature() {
   currentTemp = 0.0;
   average = 0.0;
-  for (int i = 0; i < 3; i++) {
-    sensors.requestTemperatures();
-    average += sensors.getTempCByIndex(0);
-    delay(10);
+  for (int i = 0; i<3; i++){
+  
+  sensors.requestTemperatures();
+  average = average + sensors.getTempCByIndex(0);
+  delay(100);
   }
   currentTemp = average / 3.0;
-  compareTemperature();
 }
 
 void compareTemperature() {
   if (setTemp < currentTemp) {
+    Serial.println("Set LOWER than current");
     turnOff();
+
   }
   if (setTemp > currentTemp) {
+    Serial.println("Set HIGHER than current");
     turnOn();
   }
 }
 
-
 void turnOn() {
   digitalWrite(relay, HIGH);
+  Serial.println("Relay ON");
 }
 
 void turnOff() {
   digitalWrite(relay, LOW);
+  Serial.println("Relay OFF");
 }
 
